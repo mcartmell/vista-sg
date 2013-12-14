bef1adf7665a88bfb26b483c6af2013443312ec7
Mike Cartmell <mike@mikec.me>
2013-12-14 19:37:01 +0800

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Vista::Application.routes.draw do
+  get '/geo/whereami' => 'geo#whereami'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- scripts/import_regions.rb
+++ scripts/import_regions.rb
@@ -5,25 +5,33 @@ client = MongoClient.new
 db = client['vista']
 
 types = {
-  'areas_l' => {key: 'REGION_N'},
-  'areas_m' => {key: 'PLN_AREA_N', parent_key: 'REGION_N'},
-  'areas_sm' => {key: 'SUBZONE_N', parent_key: 'PLN_AREA_N'}
+  'areas_l' => {key: 'REGION_N', size: 1},
+  'areas_m' => {key: 'PLN_AREA_N', parent_key: 'REGION_N', size: 2},
+  'areas_sm' => {key: 'SUBZONE_N', parent_key: 'PLN_AREA_N', size: 3}
 }
+
+coll = db['areas']
+coll.drop
+coll.ensure_index([[:geometry, Mongo::GEO2DSPHERE]])
 
 types.each do |size, v|
   # initialize collection
-  coll = db[size.to_s]
-  coll.drop
-  coll.ensure_index([[:geometry, Mongo::GEO2DSPHERE]])
 
   fn = File.dirname(__FILE__) + "/../data/geo/#{size}.geojson"
   j = JSON.load(File.open(fn, 'r'))
   name_key = v[:key]
+  parent_key = v[:parent_key]
   j['features'].each do |area|
     area_name = area['properties'][name_key].split(/ /).map{|s| s.capitalize}.join(' ')
     parent_name = area['properties'][parent_key].split(/ /).map{|s| s.capitalize}.join(' ') rescue nil
     #area['geometry']['coordinates'][0].uniq! {|e| "#{e[0]}_#{e[1]}"}
     coords = area['geometry']
+    defaults = {
+      name: area_name,
+      parent: parent_name,
+      size: v[:size]
+    }
+
     if coords['type'] == 'MultiPolygon'
       coords['coordinates'].each_with_index do |poly, i|
         coords = {
@@ -31,23 +39,19 @@ types.each do |size, v|
           coordinates: poly
         }
         begin
-          coll.insert({
-            name: area_name,
+          coll.insert(defaults.merge({
             geometry: coords,
-            parent: parent_name,
             poly: i
-          })
+          }))
         rescue StandardError => e
           puts "Fail #{size} #{area_name} #{i}:#{e.message[0..200]}...\n\n#{coords.to_json}\n\n"
         end
       end
     else 
       begin
-        coll.insert({
-          name: area_name,
-          geometry: coords,
-          parent: parent_name
-        })
+        coll.insert(defaults.merge({
+          geometry: coords
+        }))
       rescue StandardError => e
         puts "Fail #{size} #{area_name}:#{e.message[0..200]}"
       end
