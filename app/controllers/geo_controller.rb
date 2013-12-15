ec611b998c882e7de23c128ca017f4283e296428
Mike Cartmell <mike@mikec.me>
2013-12-15 14:59:40 +0800

    fix

--- app/controllers/geo_controller.rb
+++ app/controllers/geo_controller.rb
@@ -3,7 +3,7 @@ class GeoController < ApplicationController
   def whereami
     lat = params[:lat].to_f
     lon = params[:lon].to_f
-    vg = Geo.new
+    vg = Vista::Geo.new
     place = vg.find_smallest_place_for_coords(lat, lon)
     r = {
       name: place['name']
