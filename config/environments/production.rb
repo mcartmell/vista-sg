b475cbb281aa90aae537483e5e3513336fdd0864
Mike Cartmell <mike@mikec.me>
2014-01-01 18:37:05 +0800

    update for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Vista::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.logger = Logger.new(STDOUT)
+  config.logger.level = Logger.const_get((ENV["LOG_LEVEL"] || "INFO").upcase)
 end
